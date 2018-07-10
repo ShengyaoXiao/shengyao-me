f10eaa55317264fe7d7d87fae0f72888fc52b3f6
Higashino <Yuu@Spencers-Mac.local>
2018-07-10 22:22:04 +0800

    change about text

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -23,11 +23,10 @@ const About = () => {
             <img className="profile-picture-about" alt="profilepic" src={profilePic} />
             <div className="about-text">
                 <span className="about-text-main">Hi! I'm <span className="about-name">Shengyao</span></span> 
-                {/* <br/><span className="about-text-subtitle">I'm software engineer.</span> */}
-                <br/><br/><span>I enjoy building things with code.</span> 
-                <br/>I am a senior student at UC San Diego studying math-cs. My interets span web develoment, creative coding, and human computer interaction.
-                <br/> Currently based in Beijing, China.
-                <br/><br/>If you want to know more: <a href="" target="_blank" rel="noopener noreferrer"><span className="about-cv">HERE is my resume.</span></a>
+                <br/><span className="about-text-subtitle">I'm a frond-end engineer</span>
+                <br/><br/><span className="about-text-belief">I don't just make things work or just make things look pretty. I make them work well.</span> 
+                <br/><br/>I am a senior student at UC San Diego studying math-cs, but my interests span web develoment, creative coding, and human computer interaction. I care about the scenes behind the web as well as the final delivery to the users. 
+                <br/><br/>If you want to know more: <a href="" target="_blank" rel="noopener noreferrer"><span className="about-resume">HERE is my resume.</span></a>
                 <br/><br/>Keep scrolling to see some of the projects I've worked on.
             </div>                
         </div>
